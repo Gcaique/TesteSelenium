adb782917b137022cc4d7836d0e2f4300ed6b460
Gcaique <caique.goncalves21@gmail.com>
2026-04-14 16:50:37 -0300

    ajustes no config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
 from dotenv import load_dotenv
 import os
 import re
-import platform
-import shutil
 from pathlib import Path
 
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -14,9 +11,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 # ============================
